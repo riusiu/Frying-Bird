0a0c0ed912e5e88a22db17c9e1a0783e825514c7
Kiliken <francescopaolomariani7@gmail.com>
2024-06-18 00:31:18 +0900

    Basic_PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,40 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D playerRB;
-    public float playerVelocity = 10f;
-    public float jumpForce = 15f;
-    Vector2 playerMovement = Vector2.zero;
-    bool asJumped = false;
-    // Start is called before the first frame update
+    [Header("Movement")]
+    public Transform orientation;
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump = true;
+
+    [Header("GroundCheck")]
+    public float playerHeight;
+    public LayerMask ground;
+
+    [Header("SpriteControl")]
+    public SpriteRenderer playerSprite;
+
+    bool grounded;
+
+    float horizontalInput;
+
+    Rigidbody2D rb;
+    Vector2 moveDirection;
+
+
+    
     void Start()
     {
-        playerRB = GetComponent<Rigidbody2D>();
-        //Debug.Log(Time.deltaTime);
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
     }
 
-    bool IsGrounded()
-    {
-        if (!asJumped && playerRB.velocity.y == 0)
-        {
-            asJumped=true;
-            return true;
-        }
-        else return false;
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        playerMovement.x = ( (100f *Time.deltaTime) * playerVelocity * Input.GetAxisRaw("Horizontal"));
-        playerRB.AddForce(playerMovement);
-        if (Input.GetAxis("Jump")!= 0 && IsGrounded()) {
-            asJumped=true;
-            playerRB.AddForce(Vector2.up * jumpForce);
+        grounded = Physics2D.Raycast(transform.position, Vector2.down, playerHeight * 0.5f + 0.2f, ground);
+
+        PlayerInput();
+        SpeedControl();
+        FlipSprite();
+
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+    private void PlayerInput() {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetAxis("Jump")!= 0 && readyToJump && grounded) { 
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
         }
+    }
+    private void MovePlayer()
+    {
+        moveDirection = orientation.right * horizontalInput;
+
+        if (grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode2D.Force);
+
+        else if (!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * airMultiplier * 10f, ForceMode2D.Force);
+    }
+
+    private void SpeedControl() {
+        Vector2 flatVet = new Vector2(rb.velocity.x, 0f);
+
+        if (flatVet.magnitude > moveSpeed)
+        {
+            Vector2 limitedVel = flatVet.normalized * moveSpeed;
+            rb.velocity = new Vector2(limitedVel.x, rb.velocity.y);
+        }
+    }
+
+    private void Jump() { 
+        rb.velocity = new Vector2 (rb.velocity.x, 0f);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
+    private void FlipSprite()
+    {
+        if (horizontalInput > 0f)
+            playerSprite.flipX = false;
+        else if (horizontalInput < 0f)
+            playerSprite.flipX = true;
     }
 }
