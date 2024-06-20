4ed8afa0d501a529b54374a0fb74aaf3ee968dd9
Taisei <TTC2450DG0002@edu.tech.ac.jp>
2024-06-20 14:31:12 +0900

    Atarihanntei
    Atari hantei wo toriaezu
    tukuttayo.

---
+++ Assets/Scripts/damage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class damage : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "enemy")
+        {
+            UnityEngine.Debug.Log("Ouch!");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
