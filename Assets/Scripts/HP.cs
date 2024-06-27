b5a04b9e4f8d8ba795637fdb6d403c73feea384a
arokawin <kiy289522@gmail.com>
2024-06-27 15:29:30 +0900

    HP
    HP

---
+++ Assets/Scripts/HP.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Hoge : MonoBehaviour
+{
+    public void Start()
+    {
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+        float width = 190;
+        float height = 20;
+
+        rectTransform.sizeDelta = new Vector2(width, height);
+
+        Debug.Log("Width: " + rectTransform.sizeDelta.x);    //10
+        Debug.Log("Height: " + rectTransform.sizeDelta.y);   //20
+    }
+    
+    
+}
