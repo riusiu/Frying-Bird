ee154ad3ff5a33b2eefe88e25b4f05c160bea237
oradataisei <TTC2450DG0002@edu.tech.ac.jp>
2024-06-27 13:57:14 +0900

    arrow
    Arrow disappearing project

--- Assets/Scripts/arrow.cs
+++ Assets/Scripts/arrow.cs
@@ -7,23 +7,24 @@ public class arrow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector3(5, 0, 0);
+        transform.position = new Vector3(6, -1, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-0.004f, 0, 0);
+        transform.Translate(-0.06f, 0, 0);
     }
 
     public class damage : MonoBehaviour
     {
-        private void OnCollisionEnter2D(Collision2D collision)
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
         {
-            if (collision.gameObject.tag == "Player")
-            {
-                Destroy(this.gameObject);
-            }
+            Destroy(gameObject);
         }
     }
 }
