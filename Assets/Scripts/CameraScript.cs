0e0556894a6380add35e13f6a3bff4bb9470cc0f
Kiliken <131953633+Kiliken@users.noreply.github.com>
2024-07-18 13:23:21 +0900

    CameraScript.cs Comments

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+	Mariani Francesco Paolo
+	-----------------------
+*/
+
 public class CameraScript : MonoBehaviour
 {
     public Transform cameraPosition;
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = cameraPosition.position;
